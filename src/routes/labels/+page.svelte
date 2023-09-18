1e57b45c7101439db42a70d0b34b68948ddd3596
michaelaboah <michaelaboah@gmail.com>
2023-09-18 10:30:16 -0400

    working on safari printing bug

--- src/routes/labels/+page.svelte
+++ src/routes/labels/+page.svelte
@@ -1,62 +1,29 @@
+
 <script lang="ts">
-  import printJS from "print-js"
-  import { cableList } from "$lib/stores/equipment";
+  import PrintPdf, { Page } from "svelte-printpdf";
+  
   let labels = Array(80).fill({
     name: "Name",
     address: "Address",
     city: "City",
   });
 
+  let print = false;
 
-  function print() {
-    printJS({
-      printable: "home",
-      type: 'html',
-      showModal: true,
-      css: "4x20-AveryLabels.css", 
-      font_size: "",
-    })
-  }
-
-  let outer: HTMLElement
-  $: console.log(outer)
 </script>
 
-  <button class="btn variant-ghost" on:click={print}>Print</button>
+<button class="btn variant-ghost" on:click={() => print = true}>Print</button>
 
+<PrintPdf bind:print={print}>
+  <div class="w-fit mx-auto bg-gray-300 grid grid-cols-4 gap-4" id="home">
+    {#each labels as label, i (i)}
+      <div class="mr-12 w-44 h-12 text-center">
+        <span class="text-xs leading-3 italic">{label.name}</span>{i+1}
+      </div>
+      {#if (i + 1) % 80 === 0}
+        <div class="clear-left block"></div>
+      {/if}
+    {/each}
+  </div>
+</PrintPdf>
 
-<div class="w-fit mx-auto bg-gray-300 grid-style" id="home" bind:this={outer}>
-  {#each labels as label, i (i)}
-    <div class="label-style rounded-md bg-white" >
-      <span class="cable-name"><p class="text-black">{label.name}</p></span>{i+1}
-      <!-- {label.address} -->
-      <!-- {label.city} -->
-    </div>
-    {#if (i + 1) % 80 === 0}
-      <div class="clear-left block"></div>
-    {/if}
-  {/each}
-</div>
-
-
-<style>
-
-.cable-name {
-  font-size: 0.5rem;
-  line-height: 0.5rem; 
-  font-style: italic;
-}
-
-
-.label-style {
-  margin-right: 0.50in;
-  width: 1.75in;
-  height: 0.50in;
-  text-align: center;
-}
-.grid-style {
-  display: grid;
-  grid-template-columns: repeat(4, minmax(0, 1fr));
-}
-
-</style>
