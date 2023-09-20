319e88c7de76407d519c5de6ed06844a4ceb03bd
michaelaboah <maboah@PlexusParc.com>
2023-09-20 18:21:38 -0400

    "added script to rename binaries according to platform"

---
+++ scripts/rename-binaries.js
@@ -0,0 +1,27 @@
+import execa from 'execa'
+import fs from 'fs'
+
+let extension = ''
+if (process.platform === 'win32') {
+  extension = '.exe'
+}
+
+async function main() {
+  const rustInfo = (await execa('rustc', ['-vV'])).stdout
+  const targetTriple = /host: (\S+)/g.exec(rustInfo)[1]
+  if (!targetTriple) {
+    console.error('Failed to determine platform target triple')
+  }
+  // fs.renameSync(
+  //   `src-tauri/binaries/wkhtmltoimage${extension}`,
+  //   `src-tauri/binaries/wkhtmltoimage-${targetTriple}${extension}`,
+  // )
+  fs.renameSync(
+    `src-tauri/binaries/wkhtmltopdf${extension}`,
+    `src-tauri/binaries/wkhtmltopdf-${targetTriple}${extension}`
+  )
+}
+
+main().catch((e) => {
+  // throw e
+})
