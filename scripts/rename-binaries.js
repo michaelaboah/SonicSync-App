4562463d5bffec4b4c1799c9d52d8f115404ad49
michaelaboah <maboah@PlexusParc.com>
2023-09-20 18:52:19 -0400

    "forgot the rename call for the image binary"

--- scripts/rename-binaries.js
+++ scripts/rename-binaries.js
@@ -12,16 +12,16 @@ async function main() {
   if (!targetTriple) {
     console.error('Failed to determine platform target triple')
   }
-  // fs.renameSync(
-  //   `src-tauri/binaries/wkhtmltoimage${extension}`,
-  //   `src-tauri/binaries/wkhtmltoimage-${targetTriple}${extension}`,
-  // )
   fs.renameSync(
+    `src-tauri/binaries/wkhtmltoimage${extension}`,
+    `src-tauri/binaries/wkhtmltoimage-${targetTriple}${extension}`,
+  )
+ fs.renameSync(
     `src-tauri/binaries/wkhtmltopdf${extension}`,
     `src-tauri/binaries/wkhtmltopdf-${targetTriple}${extension}`
   )
 }
 
 main().catch((e) => {
-  // throw e
+  console.error(e)
 })
