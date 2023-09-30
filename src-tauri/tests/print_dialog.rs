067e00602595b3819c0b9b00df6d0e608d30d5d1
michaelaboah <michaelaboah@gmail.com>
2023-09-30 15:45:55 -0400

    "making progress on labels"

--- src-tauri/tests/print_dialog.rs
+++ src-tauri/tests/print_dialog.rs
@@ -1,121 +1,172 @@
-#[macro_use]
-extern crate objc;
+use genpdf::elements::{FrameCellDecorator, PaddedElement, Paragraph, Text};
+use genpdf::style::{Style, StyledString};
+use genpdf::*;
 
-use base64::engine::{self, general_purpose};
-use std::ffi;
+#[derive(Default)]
+struct AveryLabelPage {
+    page: usize,
+}
 
-use cocoa::base::id;
-#[cfg(target_os = "macos")]
-use helper::NSString;
-use objc::{
-    class,
-    declare::ClassDecl,
-    msg_send,
-    rc::StrongPtr,
-    runtime::{Object, Sel, BOOL},
-    sel, sel_impl,
-};
+impl PageDecorator for AveryLabelPage {
+    fn decorate_page<'a>(
+        &mut self,
+        context: &Context,
+        mut area: render::Area<'a>,
+        style: style::Style,
+    ) -> Result<render::Area<'a>, error::Error> {
+        self.page += 1;
+
+        area.add_margins(Margins::trbl(6.5 * 2.0, 6.5, 6.5 * 2.0, 6.9));
+
+        Ok(area)
+    }
+}
+
+#[derive(Default)]
+struct AveryLabel {
+    title: StyledString,
+    desc: StyledString,
+    source: StyledString,
+    bundle: StyledString,
+    connector: StyledString,
+    alignment: Alignment,
+}
+
+impl AveryLabel {
+    pub fn new(s: impl Into<StyledString>) -> Self {
+        Self {
+            title: s.into(),
+            ..Default::default()
+        }
+    }
+
+    fn set_alignment(&mut self, alignment: Alignment) {
+        self.alignment = alignment;
+    }
+
+    fn get_offset(&self, width: Mm, max_width: Mm, alignment: Alignment) -> Mm {
+        match self.alignment {
+            Alignment::Left => Mm::default(),
+            Alignment::Center => (max_width - width) / 2.0,
+            Alignment::Right => max_width - width,
+        }
+    }
+}
+
+impl Element for AveryLabel {
+    fn render(
+        &mut self,
+        context: &Context,
+        mut area: render::Area<'_>,
+        style: style::Style,
+    ) -> Result<RenderResult, error::Error> {
+        let StyledString {
+            ref s,
+            ref mut style,
+        } = self.title;
+
+        // area.add_margins(Margins::trbl(0.0, -3.35, 0.0, 6.35));
+
+        area.set_height(12.7.into());
+        area.set_width(44.45.into());
+
+        let mut result = RenderResult::default();
+        let Context { font_cache, .. } = &context;
+
+        // Top section
+
+        let line_style = Style::new().with_color(style::Color::Rgb(0, 0, 100));
+        for i in 0..=3 {
+            let points = vec![
+                Position::new(0.0, i as f64),
+                Position::new(area.size().width, i as f64),
+            ];
+            area.draw_line(points, line_style);
+        } // .with_font_size(60).line_height(font_cache.clone().)
+
+        if let Some(mut section) = area.text_section(font_cache, Position::default(), *style) {
+            // area.add_margins(Margins::trbl(0.0, 6.35, 0.0, 6.35));
+            let style = style
+                .with_color(style::Color::Rgb(0, 0, 0))
+                .with_font_size(10);
+            section.print_str("Title", style).unwrap();
+        }
+
+        if let Some(mut section) = area.text_section(font_cache, Position::new(0.0, 3.0), *style) {
+            // area.add_margins(Margins::trbl(0.0, 6.35, 0.0, 6.35));
+            let style = style
+                .with_color(style::Color::Rgb(255, 255, 255))
+                .with_font_size(10);
+            section.print_str("Description?", style).unwrap();
+            // assert!(section.add_newline());
+            // section.print_str("Description 2?", style).unwrap();
+        }
+
+        if let Some(mut section) = area.text_section(font_cache, Position::new(0.0, 6.0), *style) {
+            // area.add_margins(Margins::trbl(0.0, 6.35, 0.0, 6.35));
+            let style = style
+                .with_color(style::Color::Rgb(100, 0, 0))
+                .with_font_size(10);
+            section.print_str("Title", style).unwrap();
+        }
+
+        if let Some(mut section) = area.text_section(font_cache, Position::new(0.0, 9.0), *style) {
+            // area.add_margins(Margins::trbl(0.0, 6.35, 0.0, 6.35));
+            let style = style
+                .with_color(style::Color::Rgb(100, 0, 0))
+                .with_font_size(10);
+            section.print_str("Description?", style).unwrap();
+        }
+
+        // let mut zones = area.split_horizontally(&[1, 1]);
+
+        // if area.print_str(font_cache, Position::new(0.0, 5.0), *style, &s)? {
+        result.size = area.size(); // } else {
+                                   // result.has_more = true;
+                                   // }
+
+        Ok(result)
+    }
+}
 
 #[test]
 fn example() {
-    let bytes = std::fs::read("example.txt").unwrap();
+    let font_family = fonts::from_files("assets/fonts/Roboto", "Roboto", None).unwrap();
+    let mut doc = Document::new(font_family);
 
-    let c = class!(NSConcretePrintOperation);
+    doc.set_paper_size(PaperSize::Letter);
+    doc.set_title("Labels");
+    let decorator = AveryLabelPage::default();
 
-    for ivars in c.instance_methods().iter() {
-        if ivars.name().name().to_lowercase().contains("run") {
-            dbg!(ivars.name().name());
-        }
-    }
+    doc.set_page_decorator(decorator);
 
-    // Allocate an instance of NSData
-    let ns_data: id = unsafe {
-        let ns_data: id = msg_send![class!(NSData), alloc];
-        let ns_data: id = msg_send![ns_data, initWithBytes:bytes.as_ptr() length:bytes.len()];
-        ns_data
-    };
+    // let mut table = elements::TableLayout::new(vec![7, 2, 7, 2, 7, 1, 7]);
+    let mut table = elements::TableLayout::new(vec![1, 1, 1, 1]);
 
-    // let superclass = class!(PDFDocument);
-}
+    table.set_cell_decorator(FrameCellDecorator::new(false, false, false));
 
-pub mod helper {
-    use cocoa::base::id;
-    use std::ffi::c_char;
-    const UTF8_ENCODING: usize = 4;
+    let mut row = table.row();
 
-    pub struct NSString(id);
+    row.push_element(AveryLabel::new("Content of Some Kind"));
 
-    impl NSString {
-        pub fn new(s: &str) -> Self {
-            // Safety: objc runtime calls are unsafe
-            NSString(unsafe {
-                let ns_string: id = msg_send![class!(NSString), alloc];
-                let ns_string: id = msg_send![ns_string,
-                            initWithBytes:s.as_ptr()
-                            length:s.len()
-                            encoding:UTF8_ENCODING];
+    row.push_element(PaddedElement::new(
+        AveryLabel::new("Content of Some Kind"),
+        Margins::trbl(0.0, 0.0, 0.0, 2.0),
+    ));
 
-                // The thing is allocated in rust, the thing must be set to autorelease in rust to relinquish control
-                // or it can not be released correctly in OC runtime
-                let _: () = msg_send![ns_string, autorelease];
+    row.push_element(PaddedElement::new(
+        AveryLabel::new("Content of Some Kind"),
+        Margins::trbl(0.0, 0.0, 0.0, 4.0),
+    ));
 
-                ns_string
-            })
-        }
+    row.push_element(PaddedElement::new(
+        AveryLabel::new("Content of Some Kind"),
+        Margins::trbl(0.0, 0.0, 0.0, 6.0),
+    ));
 
-        pub fn to_str(&self) -> &str {
-            unsafe {
-                let bytes: *const c_char = msg_send![self.0, UTF8String];
-                let len = msg_send![self.0, lengthOfBytesUsingEncoding: UTF8_ENCODING];
-                let bytes = std::slice::from_raw_parts(bytes as *const u8, len);
-                std::str::from_utf8_unchecked(bytes)
-            }
-        }
+    row.push().unwrap();
 
-        #[allow(dead_code)] // only used when `mac-proxy` feature is enabled
-        fn to_cstr(&self) -> *const c_char {
-            unsafe {
-                let utf_8_string = msg_send![self.0, UTF8String];
-                utf_8_string
-            }
-        }
+    doc.push(table);
 
-        fn as_ptr(&self) -> id {
-            self.0
-        }
-    }
-
-    impl From<NSData> for NSString {
-        fn from(value: NSData) -> Self {
-            Self(unsafe {
-                let ns_string: id = msg_send![class!(NSString), alloc];
-                let ns_string: id = msg_send![ns_string, initWithData:value encoding:UTF8_ENCODING];
-                let _: () = msg_send![ns_string, autorelease];
-
-                ns_string
-            })
-        }
-    }
-
-    pub struct NSData(id);
-
-    impl NSData {
-        pub fn new(id: id) -> Self {
-            Self(id)
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn ns_string() {
-        let s = "hello";
-
-        let ns = NSString::new(s);
-
-        assert_eq!(s, ns.to_str())
-    }
+    doc.render_to_file("test_pdf.pdf").unwrap();
 }
